0e5b09eea5f1fa432b5241c7facd94f277bb7c2c
PierreNat <pierre.cuony@students.unibe.ch>
2019-03-27 16:04:09 +0100

    able to render 2 objects in 1 png and create the mask

--- render_2meshes2single_pgn.py
+++ render_2meshes2single_pgn.py
@@ -42,17 +42,17 @@ def main():
     textures = torch.ones(1, faces.shape[1], texture_size, texture_size, texture_size, 3, dtype=torch.float32).cuda()
 
     # to gpu
-#---------------------------------------------------------------------------------
-#extrinsic parameter, link world/object coordinate to camera coordinate
-#---------------------------------------------------------------------------------    
+# ---------------------------------------------------------------------------------
+# extrinsic parameter, link world/object coordinate to camera coordinate
+# ---------------------------------------------------------------------------------
     alpha = 0
     beta = 0
     gamma = 0
  
-    resolutionX = 512 #in pixel
+    resolutionX = 512 # in pixel
     resolutionY = 512
     scale = 1
-    f = 35 #focal on lens 
+    f = 35 # focal on lens
     sensor_width = 32 # in mm given in blender , camera sensor type
     pixels_in_u_per_mm = (resolutionX*scale)/sensor_width
     pixels_in_v_per_mm = (resolutionY*scale)/sensor_width
@@ -86,7 +86,7 @@ def main():
     t_2 = np.array([-1, 2, 5])
 
     R = np.repeat(R[np.newaxis, :, :], batch, axis=0) # shape of [batch=1, 3, 3]
-    t_1 = np.repeat(t_1[np.newaxis, :], 1, axis=0)# shape of [1, 3]
+    t_1 = np.repeat(t_1[np.newaxis, :], 1, axis=0)  # shape of [1, 3]
     t_2 = np.repeat(t_2[np.newaxis, :], 1, axis=0)
 
 # ---------------------------------------------------------------------------------
@@ -104,17 +104,19 @@ def main():
     renderer = nr.Renderer(image_size=512, camera_mode='projection',dist_coeffs=None, K=K, R=R, t=t_1, near=0.1, far=1000, orig_size=512)
     renderer2 = nr.Renderer(image_size=512, camera_mode='projection', dist_coeffs=None, K=K, R=R, t=t_2, near=0.1,far=1000, orig_size=512)
 
+
+
 # render object
 
     nb_obj2render = 2
 
-    loop = tqdm.tqdm(range(0, 1))
+    loop = tqdm.tqdm(range(0, 1, 1))
     writer = imageio.get_writer(args.filename_output, mode='i')
 
     for num, obj in enumerate(loop):
         loop.set_description('Rendering objects')
 
-        if nb_obj2render == 1: # render 1 3d object
+        if nb_obj2render == 1:  # render 1 3d object
             images_1 = renderer(vertices, faces, textures)  # [batch_size, RGB, image_size, image_size]
             image = images_1[0].detach().cpu().numpy()[0].transpose((1, 2, 0))
 
@@ -130,25 +132,25 @@ def main():
 
     writer.close()
 
-# # render object silhouette
-#     angle= 0
-#     loop = tqdm.tqdm(range(0, 1))
-#     writer = imageio.get_writer(args.filename_output2, mode='i')
-#     for num, azimuth in enumerate(loop):
-#         loop.set_description('Rendering objects' silhouettes')
-#
-#         images_1 = renderer(vertices, faces, textures, mode='silhouettes')  # [batch_size, RGB, image_size, image_size]
-#         images_2 = renderer2(vertices, faces, textures)
-#
-#         # render silhouette:
-#         image = images_1.detach().cpu().numpy().transpose((1, 2, 0))
-#         # render 2 3d objects
-#         # image = np.minimum(images_1[0].detach().cpu().numpy()[0].transpose((1, 2, 0)), images_2[0].detach().cpu().numpy()[0].transpose((1, 2, 0)))
-#         # image[np.int(resolutionX/2),np.int(resolutionY/2)] = [1,1,1] #draw middle point camera center
-#
-#
-#         writer.append_data((255*image).astype(np.uint8))
+# render object silhouette
+    loop = tqdm.tqdm(range(0, 1, 1))
+    writer = imageio.get_writer(args.filename_output2, mode='i')
+    for num, azimuth in enumerate(loop):
+        loop.set_description('Rendering objects silhouettes')
+
+        if nb_obj2render == 1:  # render 1 3d object
+            images_1 = renderer(vertices, faces, textures, mode='silhouettes')  # [batch_size, RGB, image_size, image_size]
+            image = images_1.detach().cpu().numpy().transpose((1, 2, 0))
+
+        else:  # render 2 3d objects
+            images_1 = renderer(vertices, faces, textures, mode='silhouettes')
+            images_2 = renderer2(vertices, faces, textures, mode='silhouettes')
+            image = np.maximum(images_1.detach().cpu().numpy().transpose((1, 2, 0)),
+                               images_2.detach().cpu().numpy().transpose((1, 2, 0)))
+
+        writer.append_data((255*image).astype(np.uint8))
+    writer.close()
 
 if __name__ == '__main__':
     main()
-    
+
